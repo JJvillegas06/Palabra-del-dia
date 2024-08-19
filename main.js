1771e755917f77c308ddf6c816b76fd80066456c
JJvillegas06 <juanjose1967@live.com>
2024-08-18 22:48:53 -0500

    agrego funcionalidad

--- main.js
+++ main.js
@@ -4,18 +4,106 @@ const listaPalabras = [
 let numeroAleatorio = Math.floor(Math.random()*listaPalabras.length)
 let palabraAleatoria = listaPalabras[numeroAleatorio]
 console.log(palabraAleatoria)
-let palabraCorrecta = [false,false,false,false,false]
+let intentos = 1
+let casillaRevisada = 0
+let contador = 0
+
+
+let fila = document.querySelectorAll('.fila')
+
 const btnEnviar = document.querySelector('#enviar')
 btnEnviar.addEventListener('click', ()=>{
+    
     let palabraUsuario = document.querySelector('#palabraUsuario').value
-    for (let i=0; i<palabraAleatoria.length;i++){
-    let espacioLetra = document.querySelectorAll('.letra')
-        espacioLetra[i].innerHTML = palabraUsuario[i]
-    if (palabraUsuario[i] == palabraAleatoria[i]){
-        palabraCorrecta[i] = true
-        console.log(palabraCorrecta)
-    }else{
-        console.log('perdiste')
+
+    const teclado = document.querySelectorAll('.teclado')
+    for(let i = 0; i < 28 ;i++){
+        teclado.addEventListener('click',()=>{
+            console.log(teclado)
+            fila[i].innerHTML =  teclado[i].innerHTML
+        })
     }
-}
-})
+    
+        
+        
+    
+        
+
+    for (casillaRevisada; casillaRevisada < intentos * 5; casillaRevisada++){
+
+        fila[casillaRevisada].innerHTML = palabraUsuario[casillaRevisada % 5]
+        
+           
+           for (let i = (intentos -1) * 5 ; i < intentos * 5; i++){
+            console.log(palabraUsuario[i % 5],palabraAleatoria[casillaRevisada % 5])
+
+
+            if(palabraAleatoria[casillaRevisada % 5] == palabraUsuario[i % 5]){
+                //fila[i].classList.remove('nada')
+                
+                fila[i].classList.add('naranja')
+            
+
+
+                if (i % 5 == casillaRevisada % 5 ){
+                    fila[casillaRevisada].classList.add('verde')
+
+                }
+            } 
+            
+
+           }
+
+           
+           
+       // }else{
+          //  console.log('perdiste')
+        // }
+           
+    }
+    
+
+    console.log(intentos)
+    
+    
+    
+    intentos ++
+
+    }   
+    
+    
+)   
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+   
+     
+    
+        
+    
+
+
+    
+
+    
+    /*
+     
+    
+    if(!saltoDeLinea){
+        for (let i=0; i < palabraAleatoria.length ;i++){
+        espacioLetra[i].innerHTML = palabraUsuario[i]
+    }*/
+
+
+    
